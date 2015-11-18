210d7fbc104c86dc59b1eee5065871530afa904e
Tony Kay <twkay@thenavisway.com>
2015-11-18 17:12:35 -0500

    added a bit of cleaner code to simulation

--- src/om_tutorial/core.cljs
+++ src/om_tutorial/core.cljs
@@ -24,14 +24,14 @@
        Object
        (render [this]
                (let [people (-> (om/props this) :people)]
-                 (let [] 
+                 (let []
                    (dom/div nil
-                           (dom/ul nil (map person people))
+                            (dom/ul nil (map person people))
                             ;IMPORTANT: THIS IS NOT A REAL ADD...SIMULATE AN ADD to the server. It will look like it works ONCE
-                           (dom/button #js {:onClick #(om/transact! this `[(app/add-person) :people (quote ~(cljs.core/first (om/get-query this)))])} "Add Person")
+                            (dom/button #js {:onClick #(om/transact! this `[(app/add-person) :people (quote ~(cljs.core/first (om/get-query this)))])} "Add Person")
                             ;IMPORTANT: THIS IS NOT A REAL DELETE...SIMULATE a DELETE...ONLY WORKS IF YOU'VE DONE the Add already, since it is faking removing the one you added
-                           (dom/button #js {:onClick #(om/transact! this '[(app/delete-person) :people])} "Delete Person")
-                           ))
+                            (dom/button #js {:onClick #(om/transact! this '[(app/delete-person) :people])} "Delete Person")
+                            ))
                  )
                )
        )
@@ -53,7 +53,7 @@
    })
 (defmethod mutate 'app/delete-person [{:keys [state] :as env} k {:keys [db/id]}]
   {:remote true
-   :action (fn [] (swap! state update-in [:people] butlast) ) ; optimistic delete...always denied by server unless "Joey"
+   :action (fn [] (swap! state update-in [:people] butlast)) ; optimistic delete...always denied by server unless "Joey"
    })
 
 (def initial-state {:people [{:db/id 1 :person/name "Sam"} {:db/id 2 :person/name "Tammy"}]})
@@ -67,20 +67,23 @@
 
 (def parser (om/parser {:read read :mutate mutate}))
 (defonce reconciler (om/reconciler {:state  initial-state
-                                :parser parser
-                                :send   (fn [query cb]
-                                          (println "REMOTE" query)
-                                          (js/setTimeout
-                                            #(if (= 'app/add-person (-> query :remote ffirst))
-                                              ; Swap comment lines below to simulate different server responses
-                                              ;(let [people-read-response pretend-added-person-state] (cb people-read-response)) ; simulate add ok
-                                              (let [people-read-response pretend-deleted-person-state] (cb people-read-response)) ; simulate add denied
-                                              (let [people-read-response pretend-deleted-person-state]
-                                                (println "DELETE") ; delete
-                                                ; call the callback, which will SIMULATE what I get back from the server (e.g. the original list)
-                                                (cb people-read-response)) ; delete
-                                              ) 1000)
-                                          )}))
+                                    :parser parser
+                                    :send   (fn [query cb]
+                                              (println "REMOTE" query)
+                                              (js/setTimeout
+                                                (fn []
+                                                  (let [is-add? (= 'app/add-person (-> query :remote ffirst))
+                                                        add-allowed? true] ; change to alter simulation of server being ok with add
+                                                    (if is-add?
+                                                      (if add-allowed?
+                                                        (let [people-read-response pretend-added-person-state] (cb people-read-response)) ; simulate add ok
+                                                        (let [people-read-response pretend-deleted-person-state] (cb people-read-response))) ; simulate add denied
+                                                      (let [people-read-response pretend-deleted-person-state]
+                                                        (println "DELETE") ; delete
+                                                        ; call the callback, which will SIMULATE what I get back from the server (e.g. the original list)
+                                                        (cb people-read-response)) ; delete
+                                                      ))) 1000)
+                                              )}))
 
 (om/add-root! reconciler Root (gdom/getElement "app"))
 
