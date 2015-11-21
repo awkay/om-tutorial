a1aeb0e836e78627e5d322408efb9ccbf1d35367
Tony Kay <twkay@thenavisway.com>
2015-11-20 16:44:46 -0800

    added more docs describing Om default database format

--- src/tutorial/om_tutorial/D_App_Database.cljs
+++ src/tutorial/om_tutorial/D_App_Database.cljs
@@ -8,6 +8,8 @@
             ))
 
 (defui C
+       static om/Ident
+       (ident [this props] [:c/by-k (:k props)])
        static om/IQuery
        (query [this] [:k :k2])
        Object
@@ -46,7 +48,7 @@
 
 (def a (om/factory A))
 
-(def state-tree {:a 1 :join-b {:b 2 :join-c [{:k 1 :k2 2} {:k 3 :k2 4}]}})
+(def result-tree {:a 1 :join-b {:b 2 :join-c [{:k 1 :k2 2} {:k 3 :k2 4}]}})
 
 (defcard-doc
   "
@@ -62,15 +64,13 @@
   The default database format is an edn map. It can have as many top-level keys as make sense to you, but in general
   there will be top-level keys for each property in the top-level query of your UI. For Om to work right, anything
   that might be shared in the UI (or is dealt with in persistence layers) should have an ident 
-  (see [the glossary](#!/om_tutorial.Z_Glossary)).
+  (see [the glossary](#!/om_tutorial.Z_Glossary)). We'll get to that in a minute.
   
   Remember from the section on Queries that when you co-locate queries on components, you may or may not 
-  have interstitial stateless components. This means your UI tree and query tree need not match.
-  
+  have interstitial stateless components. 
   The default database format is structured like the *query tree*. Anything that has a co-located query
-  should appear in the application state tree. If the node(s) in the UI tree that correlate with the query
-  have an Ident, the the actual state object should be placed in top-level tables, and the state *tree* should
-  contain the Ident instead (as a reference to the potentially shared data).
+  should appear in the application state tree. 
+  This means your UI tree and query tree almost certainly will not match.
   
   For example, if you had the following UI elements:
   
@@ -89,41 +89,58 @@
   "
   (om/get-query A)
   "
-  then one possible state tree might be:
+  then one possible *result* tree for this UI might be:
   "
-  state-tree
+  result-tree
   "
   and the resulting rendering of this would be:
-  
   ")
 
 (defcard rendering-of-A
          (fn [state-atom _]
-           (a state-tree)
+           (a result-tree)
            )
-         state-tree
+         result-tree
          {:inspect-data true}
          )
 
 (defcard-doc
   "
-  but the overall UI tree has an intermediate (stateless) component between B and C.
+  So while the overall UI tree has `I` in it, the query result tree does not (since `I` has no query).
   
-      A
-      |
-      B
-      |
-      I  <- stateless UI. No query.
-      |
-      C
-     /|\\
-    D D D
-    
-  in code this would be:
+  But we're not done yet. What if two components on the screen happen to render different views of the same 
+  data? Any time you pull information from a server, it is possible someone might co-locate a query for it, and
+  then you're screwed...updating one branch of the UI state tree will only cause one component to update.
+  
+  Identity (and cross-referencing) to the rescue.
+  
+  By placing an Ident on your component, you enable Om to understand when two components are rendering data derived
+  from the same source (e.g. a table and a graph).
+  
+  In order for this to work well, you ideally want to have that shared data in your app state just once.
+  
+  An ident is just a unique identity, represented as a 2-tuple `vector` with a first element keyword. An ident need only
+  be client-unique, but will often be based on real server-persisted data. Examples might be `[:people/by-id 3]`
+  and `[:ui.button/by-id 42]`. Om can use these to find components that share state and should update together,
+  and for other things like parse optimization.
+  
+  So, the default Om database format requires that any component with an Ident have it's state represented in the 
+  state database as the ident of the object, and the actual data for that object goes at the *top* of the state
+  map, as a database map of those kind of objects. The Om function `tree->db` can convert such database *state* 
+  trees into this format given a query from components that have `Ident`. 
+  
+  If we take the proposed *result tree*:"
+  result-tree
+  "
+  and apply `(om/tree->db A result-tree true)`:"
+  (om/tree->db A result-tree true)
+  "
+  
+  Then we arrive at the desired default database format for Om.
+  
+  Formally, this is a tree of data where all of the objects that have an ident are replaced by that ident, and 
+  the actual data of those objects is moved to top-level Om-owned tables.
   
   ## TODO: Diagram of default database format
   
-  ## Ident
-  
-  Components can be given an Ident 
   ")
--- src/tutorial/om_tutorial/Z_Glossary.cljs
+++ src/tutorial/om_tutorial/Z_Glossary.cljs
@@ -10,8 +10,12 @@
 (defcard-doc
   "# Glossary of Terms
   
+  - `Default database format`: A tree of data where all of the objects that have an ident are replaced by that ident, and 
+    the actual data of those objects is moved to top-level Om-owned tables. See `om/tree->db`.
   - `Ident`: A unique identity, represented as a 2-tuple `vector` with a first element keyword. An ident need only
     be client unique, but will often be based on real server-persisted data. Examples might be `[:people/by-id 3]`
     and `[:ui.button/by-id 42]`. Om can use these to find components that share state and should update together,
     and for other things like parse optimization.
+  - `Stateless Component`: A component defined by `defui` that has no query. Such components must not change the structure
+  of the incoming props (they must pass them down without removing structure).
   ")
