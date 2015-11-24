7b2d7e356eddd4450080edd3c482230362baa134
Tony Kay <twkay@thenavisway.com>
2015-11-24 11:47:11 -0800

    changed how I stop recursion

--- src/main/om_tutorial/local_read.cljs
+++ src/main/om_tutorial/local_read.cljs
@@ -9,31 +9,32 @@
     :people/by-id {:value (p/parse-join-with-reader read-local (assoc env :db-path []) (:key ast))}
     :ui.people/checked (p/ui-attribute env key)
     ;; needed to bump recursion limit due to possible path optimization path starting point
-    :person/mate {:value (p/parse-join-with-reader read-local env key :limit 3)}
+    :person/mate (when-not (:mate-read env) {:value (p/parse-join-with-reader read-local (assoc env :mate-read true) key)})
     :people {:value (p/parse-join-with-reader read-local env key)}
-    :widget {:value (p/parse-join-with-reader read-local env key :reset-depth 0)}
+    :widget {:value (p/parse-join-with-reader read-local env key)}
     (p/db-value env key)
     ))
 
-(defn generic-read-local
-  "EXPERIMENTAL: A function used by an Om parser to read local app state. This should work for lots of things except deep nesting
-  and unions (not supported yet by my helpers). NOTE: This also supports using the special ui-attribute db store that
-  I invented for decomplecting UI state from persistent state"
-  [{:keys [ast] :as env} key params]
-  (println "READ " key)
-  (case key
-    ; path optimization...TODO: generalize
-    :people/by-id {:value (p/parse-join-with-reader read-local (assoc env :db-path []) (:key ast))}
-    (let [is-ui? (and (string? (namespace key)) (re-matches #"^ui\..*" (namespace key)))
-          is-join? (= :join (:type ast))]
-      (cond
-        is-ui? (p/ui-attribute env key)
-        is-join? {:value (p/parse-join-with-reader generic-read-local env key :limit 3)}
-        :else (p/db-value env key)
-        ))))
 
-;; NOTE: The following versions have not been updated to support pathopt
 (comment
+  (defn generic-read-local
+    "EXPERIMENTAL: A function used by an Om parser to read local app state. This should work for lots of things except deep nesting
+    and unions (not supported yet by my helpers). NOTE: This also supports using the special ui-attribute db store that
+    I invented for decomplecting UI state from persistent state"
+    [{:keys [ast] :as env} key params]
+    (println "READ " key)
+    (case key
+      ; path optimization...TODO: generalize
+      :people/by-id {:value (p/parse-join-with-reader read-local (assoc env :db-path []) (:key ast))}
+      (let [is-ui? (and (string? (namespace key)) (re-matches #"^ui\..*" (namespace key)))
+            is-join? (= :join (:type ast))]
+        (cond
+          is-ui? (p/ui-attribute env key)
+          is-join? {:value (p/parse-join-with-reader generic-read-local env key :limit 3)}
+          :else (p/db-value env key)
+          ))))
+
+  ;; NOTE: The following versions have not been updated to support pathopt
   (defn split-read-person "Read stuff for a person query." [env key params]
     (case key
       :ui/checked (p/ui-attribute env key)
