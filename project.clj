dc1d99e5387473f7db7a59a31f12c6529a723afb
Tony Kay <twkay@thenavisway.com>
2015-11-25 22:44:47 -0800

    bumped om version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
                  [org.clojure/clojurescript "1.7.170" :scope "provided"]
                  [devcards "0.2.1" :exclusions [org.omcljs/om]]
-                 [org.omcljs/om "1.0.0-alpha23"]
+                 [org.omcljs/om "1.0.0-alpha24"]
                  [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "test"]]
 
   :source-paths ["src/main" "src/cards" "src/tutorial"]
