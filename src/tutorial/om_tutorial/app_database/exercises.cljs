383fa49ec3a7ed14832d3c27c9a1d4d5a2f21992
Tony Kay <twkay@thenavisway.com>
2015-12-11 23:39:03 -0800

    Added app database exercises and solutions.

---
+++ src/tutorial/om_tutorial/app_database/exercises.cljs
@@ -0,0 +1,17 @@
+(ns om-tutorial.app-database.exercises)
+
+(def cars-table
+  {
+   ; TODO (exercise 1): Add a :cars/by-id table
+   })
+
+(def favorites
+  ; TODO (exercise 2): merge your cars table from above here
+  {
+   ; TODO (exercise 2): Add a :favorite-car key that points to the Nissan Leaf via an ident
+   })
+
+(def ex3-uidb
+  {
+   ; TODO (exercise 3): Add tables. See exercise text.
+   })
