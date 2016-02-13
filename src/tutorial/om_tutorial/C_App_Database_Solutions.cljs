2730dd41565a9f3f319b6a7daa7565c4c4c3c9e9
Tony Kay <twkay@thenavisway.com>
2016-02-12 16:24:48 -0800

    added soultions to database exercises

---
+++ src/tutorial/om_tutorial/C_App_Database_Solutions.cljs
@@ -0,0 +1,26 @@
+(ns om-tutorial.C-App-Database-Solutions)
+
+(def cars-table
+  {:cars/by-id {
+                1 {:id 1 :make "Nissan" :model "Leaf"}
+                2 {:id 2 :make "Dodge" :model "Dart"}
+                3 {:id 3 :make "Ford" :model "Mustang"}
+                }
+   })
+
+(def favorites
+  (merge cars-table
+    {
+     :favorite-car [:cars/by-id 1]
+     }))
+
+(def ex3-uidb
+  {
+   :tools/by-id {1 {:id 1 :label "Cut"}
+                 2 {:id 2 :label "Copy"}}
+   :data/by-id  {5 {:id 5 :x 1 :y 3}}
+   :toolbar     {:main {:tools [[:tools/by-id 1] [:tools/by-id 2]]}}
+   :canvas      {:main {:data [[:data/by-id 5]]}}
+   :main-panel  {:toolbar [:toolbar :main]
+                 :canvas  [:canvas :main]}
+   })
